c29605023e51727e81724941cc40b9c5a35f4768
Jee <48724771+imjee@users.noreply.github.com>
2025-07-03 13:06:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+fetch('https://your-backend-url.com/api/products')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('produk-container');
+    data.forEach(p => {
+      const div = document.createElement('div');
+      div.className = 'produk';
+      div.innerHTML = `<h3>${p.nama}</h3><p>Rp ${p.harga}</p><button>Beli</button>`;
+      container.appendChild(div);
+    });
+  });
